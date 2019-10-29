64196662065d0f356a4a344a139555a7b7686577
wuwenyuan <694232592@qq.com>
2019-10-29 21:04:23 +0800

    commit

--- app/src/main/java/com/example/finalapp/RateActivity.java
+++ app/src/main/java/com/example/finalapp/RateActivity.java
@@ -1,17 +1,26 @@
 package com.example.finalapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class RateActivity extends AppCompatActivity {
+
+    private final String TAG = "Rate";
+    private float dollarRate = 0.1f;
+    private float euroRate = 0.2f;
+    private float wonRate = 0.3f;
 
     EditText rmb;
     TextView show;
@@ -27,7 +36,10 @@ public class RateActivity extends AppCompatActivity {
 
     public void onClick(View btn) {
         //获取用户输入内容
+        Log.i(TAG, "onClick: ");
         String str = rmb.getText().toString();
+        Log.i(TAG, "onClick: get str=" + str);
+        
         float r = 0;
         if (str.length()>0) {
             r = Float.parseFloat(str);
@@ -36,24 +48,69 @@ public class RateActivity extends AppCompatActivity {
             Toast.makeText(this,"请输入金额",Toast.LENGTH_SHORT).show();
         }
 
-        float val = 0;
+        Log.i(TAG, "onClick: r=" + r);
+
+        //float val = 0;
         if (btn.getId()==R.id.btn_dollar) {
-            val = r*(1/6.7f);
+            show.setText(String.format("%.2f",r*dollarRate));
         }else if (btn.getId()==R.id.btn_euro) {
-            val = r*(1/11f);
+            show.setText(String.format("%.2f",r*euroRate));
         }else{
-            val = r*500;
+            show.setText(String.format("%.2f",r*wonRate));
         }
-        show.setText(String.valueOf(val));
+        //show.setText(String.valueOf(val));
     }
 
     public void openOne(View btn) {
-        //打开一个页面Activity
-        Log.i("open", "openOne: ");
-        Intent hello = new Intent(this,SecondActivity.class);
-        Intent web = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.jd.com"));
-        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:87092173"));
-        //startActivity(intent);
+        openConfig();
 
+    }
+
+    private void openConfig() {
+        //打开一个页面Activity
+        //Log.i("open", "openOne: ");
+        Intent config = new Intent(this, ConfigActivity.class);
+        config.putExtra("dollar_rate_key", dollarRate);
+        config.putExtra("euro_rate_key", euroRate);
+        config.putExtra("won_rate_key", wonRate);
+
+        Log.i(TAG, "openOne: dollar_rate_key=" + dollarRate);
+        Log.i(TAG, "openOne: euro_rate_key=" + euroRate);
+        Log.i(TAG, "openOne: won_rate_key=" + wonRate);
+
+        //startActivity(config);
+        startActivityForResult(config, 1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.rate,menu);
+        return true;
+}
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()==R.id.menu_set){
+            openConfig();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode==1 &&resultCode==2){
+            /*  bdl.putFloat("key_dollar",newDollar);
+                bdl.putFloat("key_euro",newEuro);
+                bdl.putFloat("key_won",newWon);*/
+            Bundle bundle = data.getExtras();
+            dollarRate = bundle.getFloat("key_dollar",0.1f);
+            euroRate = bundle.getFloat("key_euro",0.1f);
+            wonRate = bundle.getFloat("key_won",0.1f);
+            Log.i(TAG, "onActivityResult: dollarRate=" + dollarRate);
+            Log.i(TAG, "onActivityResult: euroRate=" + euroRate);
+            Log.i(TAG, "onActivityResult: wonRate=" + wonRate);
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
